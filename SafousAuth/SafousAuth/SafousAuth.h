73da8c0f4b9eb92d21cb7d5b616dff096dfe602e
Bangkit Purbandoro <>
2023-03-01 20:45:32 +0700

    feat: add init data

---
+++ SafousAuth/SafousAuth/SafousAuth.h
@@ -0,0 +1,18 @@
+//
+//  SafousAuth.h
+//  SafousAuth
+//
+//  Created by Bangkit on 01/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SafousAuth.
+FOUNDATION_EXPORT double SafousAuthVersionNumber;
+
+//! Project version string for SafousAuth.
+FOUNDATION_EXPORT const unsigned char SafousAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafousAuth/PublicHeader.h>
+
+
---
+++ SafousAuth/SafousAuth/SafousAuth.swift
@@ -0,0 +1,8 @@
+//
+//  SafousAuth.swift
+//  SafousAuth
+//
+//  Created by Bangkit on 01/03/23.
+//
+
+import Foundation
